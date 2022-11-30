1b1692a50a36436f976747e14962f23a5b661e84
AntonioSalto <a.saltoborso@gmail.com>
2022-11-30 23:56:39 +0100

    creando insider scores dataset

--- .gitignore/pruebas.py
+++ .gitignore/pruebas.py
@@ -1,9 +1,13 @@
-import pandas as pd 
-import numpy as np 
-df = pd.DataFrame(
-    [[0,0,0],
-    [0,0,3],
-    [1,5,7],
-    [8,7,3]],
-    columns=['c1','c2','c3'])
-print(df.loc[df['c2'].ne(0), 'c2'].first_valid_index())
+ticker = complements[cik]['ticker']
+		stock = yf.Ticker(ticker)
+		prices = stock.history(period='max')
+		if prices['Open']['2010-01-01':].any() == False or 'P' not in transactions[cik]['nonDerivative']['A']:
+			continue
+		#p se refiere obviamente a purchase
+		cik_ins_p = transactions[cik]['nonDerivative']['A']['P']
+		cik_ins_p = pd.DataFrame(cik_ins_p.loc[cik_ins_p.ne(0)])
+		cik_ins_p.rename(columns={'P': complements[cik]['ticker']}, inplace=True)
+		print()
+		print(type(cik_ins_p.index.values.tolist()[0]))
+		p_prices = [prices[date] for date in cik_ins_p.index.values.tolist()]
+		df.loc[:, [complements[cik]['ticker']]] *= np.array(p_prices)
--- analysis.py
+++ analysis.py
@@ -1,4 +1,4 @@
-#cambio de prueba
+#lo pongo aqui arriba para que no se vea de la verguenza que da: las horas en yfinance son de españa y la sec lo pone en horarios usa crac
 import requests
 import urllib
 from bs4 import BeautifulSoup
@@ -11,6 +11,7 @@ import yfinance as yf
 from tkinter import *
 import inputs_management
 import solapas
+import numpy as np
 
 def open_json(dict_name):
     with open(dict_name, 'r') as fp:
@@ -36,13 +37,20 @@ def insider_score():
 		ticker = complements[cik]['ticker']
 		stock = yf.Ticker(ticker)
 		prices = stock.history(period='max')
-		if prices['Open']['2010-01-01':].any() == False:
+		if prices['Open']['2010-01-01':].any() == False or 'P' not in transactions[cik]['nonDerivative']['A']:
 			continue
-		if 'P' in transactions[cik]['nonDerivative']['A']:
-			cik_ins_p = transactions[cik]['nonDerivative']['A']['P']
-			print(cik_ins_p.loc[cik_ins_p.ne(0)])
-		else:
-			continue
+		#p se refiere obviamente a purchase
+		cik_ins_p = transactions[cik]['nonDerivative']['A']['P']
+		cik_ins_p = pd.DataFrame(cik_ins_p.loc[cik_ins_p.ne(0)])
+		cik_ins_p.rename(columns={'P': complements[cik]['ticker']}, inplace=True)
+		try:
+			cik_ins_p['PURCHASE PRICE'] = [prices['Open'][date] for date in list(cik_ins_p.index.values)]
+			cik_ins_p['COST'] = cik_ins_p[complements[cik]['ticker']] * cik_ins_p['PURCHASE PRICE']
+			print(cik_ins_p)
+		except:
+			print(cik_ins_p)
+			print('CAGO EN LA HOSTIA YA')
+			pass
 	return 1
 
 def plot_all_graphs():
@@ -70,7 +78,6 @@ def analyse_form_4_main(textBox1, textBox2, textBox3, textBox4):
 	global complements, transactions
 	if (inputs := inputs_management.retrieve_analysis_inputs(textBox1, textBox2, textBox3, textBox4, run = True))['keep_downloading'] == False:
 		transactions = group_by_tr_type(json_to_df(solapas.merge_solapas_in_df(inputs)))
-		print(transactions)
 		complements = open_json('.gitignore/data/complements.json')
 		insider_score()
 		#plot_all_graphs()
